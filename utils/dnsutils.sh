315c412039d82c4632a91db6f9ceb3da9650184a
chukmunnlee <chukmunnlee@gmail.com>
2021-11-11 12:08:03 +0800

    day3 am demos

--- utils/dnsutils.sh
+++ utils/dnsutils.sh
@@ -1,2 +1,7 @@
 #!/usr/bin/env bash
-kubectl run dnsutils --image=gcr.io/kubernetes-e2e-test-images/dnsutils:1.3 --restart=Never --command sleep 10000000
+kubectl run dnsutils \
+	--image=gcr.io/kubernetes-e2e-test-images/dnsutils:1.3 \
+	--restart=Never \
+	--command sleep 10000000
+	#-o yaml --dry-run=client
+
